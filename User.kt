f9e5e89c7a0b4676ea328d70d96526e75c5bddf0
Vladimir Polozov <z2005q.polozov@yandex.ru>
2025-09-10 11:01:19 +0700

    create class User, hash pass func

---
+++ User.kt
@@ -0,0 +1,24 @@
+import java.security.MessageDigest
+import java.security.SecureRandom
+
+class User(val login: String, val password: String) {
+    val passwordHash: ByteArray
+    val salt: ByteArray = ByteArray(16).also { SecureRandom().nextBytes(it) }
+
+    init {
+        passwordHash = hashPasswordWithSalt(password, salt)
+    }
+
+    private fun hashPasswordWithSalt(password: String, salt: ByteArray): ByteArray {
+        val md = MessageDigest.getInstance("SHA-256")
+        val passwordBytes = password.toByteArray(Charsets.UTF_8)
+        md.update(salt) // солим пароль
+        md.update(passwordBytes)
+        return md.digest()
+    }
+
+    fun checkPassword(attempt: String): Boolean {
+        val attemptHash = hashPasswordWithSalt(attempt, salt)
+        return MessageDigest.isEqual(passwordHash, attemptHash)
+    }
+}
