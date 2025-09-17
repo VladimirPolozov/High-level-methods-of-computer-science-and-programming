5364be5b3fae7ea92c51179ac8e8138b184c4b01
inkass <Ponovadim67@mail.ru>
2025-09-17 06:57:07 +0300

    add data class for resoures and create them

--- Resourse.kt
+++ Resourse.kt
@@ -2,24 +2,8 @@ data class Resource(
     val name: String,
     val maxVolume: Int,
     val content: String? = null,
-    val children: MutableList<Resource>
+    val children: MutableList<Resource> = mutableListOf()
 )
 
-fun resource(name: String, max: Int, content: String? = null, init: MutableList<Resource>.() -> Unit = {}): Resource {
-    val children = mutableListOf<Resource>()
-    children.init()
-    return Resource(name, max, content, children)
-}
 
-val root = resource("ROOT", Int.MAX_VALUE) {
-    add(resource("A", 100) {
-        add(resource("B", 80) {
-            add(resource("C", 50) {
-                add(resource("f_d", 30))
-            })
-            add(resource("G", 20))
-        })
-        add(resource("A8B", 40))
-    })
-}
 
