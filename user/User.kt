7d08e2f2d7cc8b1ddd16d93ef746cddb23a6356d
Vladimir Polozov <z2005q.polozov@yandex.ru>
2025-09-24 10:37:18 +0700

    optimize permissions functionality, add CreateUsers.kt

--- user/User.kt
+++ user/User.kt
@@ -3,12 +3,15 @@ package user
 import java.security.MessageDigest
 import java.security.SecureRandom
 
-// добавить права доступа к ресурсам
-class User(val login: String, val password: String) {
+data class Permissions(val path: String, val actions: Set<String>)
+
+class User(
+    val login: String,
+    val password: String,
+    val permissions: List<Permissions> = emptyList()) {
+
     val passwordHash: ByteArray
     val salt: ByteArray = ByteArray(16).also { SecureRandom().nextBytes(it) }
-
-    private val permissions = mutableListOf<Pair<String, MutableSet<String>>>()
 
     init {
         passwordHash = hashPasswordWithSalt(password, salt)
@@ -28,26 +31,18 @@ class User(val login: String, val password: String) {
         return MessageDigest.isEqual(passwordHash, attemptHash)
     }
 
-    fun setPermission(path: String, action: String) {
-        val action = action.lowercase()
-        val entry = permissions.find { it.first == path }
-        if (entry != null) {
-            entry.second.add(action)
-        } else {
-            permissions.add(path to mutableSetOf(action))
-        }
-    }
-
     fun checkPermission(path: String, action: String): Boolean {
-        val action = action.lowercase()
+        val normAction = action.lowercase()
         val parts = path.split('.')
         val sb = StringBuilder()
         for (i in parts.indices) {
             if (i > 0) sb.append('.')
             sb.append(parts[i])
             val prefix = sb.toString()
-            val entry = permissions.find { it.first == prefix }
-            if (entry != null && action in entry.second) {
+
+            if (permissions.any {
+                    it.path == prefix && normAction in it.actions.map { a -> a.lowercase() }
+                }) {
                 return true
             }
         }
