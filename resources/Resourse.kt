60ac3a903a25cf85aa2e6c9782b345a5cb9af593
Vladimir Polozov <z2005q.polozov@yandex.ru>
2025-09-24 10:18:21 +0700

    add find method to Resourse.kt

--- resources/Resourse.kt
+++ resources/Resourse.kt
@@ -5,7 +5,18 @@ data class Resource(
     val maxVolume: Int,
     val content: String? = null,
     val children: MutableList<Resource> = mutableListOf()
-)
+) {
+    fun findResource(path: String): Resource? {
+        if (path.isEmpty()) return this
 
+        val parts = path.split('.')
+        var current: Resource = this
 
-
+        for (part in parts) {
+            val child = current.children.find { it.name == part }
+                ?: return null // путь не существует
+            current = child
+        }
+        return current
+    }
+}
