da8ca1f3addfd592ad22ac6c124c42dece7f180c
inkass <Ponovadim67@mail.ru>
2025-09-28 10:25:17 +0300

    change main.kt + build files

---
+++ src/resources/Resourse.kt
@@ -0,0 +1,22 @@
+package resources
+
+data class Resource(
+    val name: String,
+    val maxVolume: Int,
+    val content: String? = null,
+    val children: MutableList<Resource> = mutableListOf()
+) {
+    fun findResource(path: String): Resource? {
+        if (path.isEmpty()) return this
+
+        val parts = path.split('.')
+        var current: Resource = this
+
+        for (part in parts) {
+            val child = current.children.find { it.name == part }
+                ?: return null // путь не существует
+            current = child
+        }
+        return current
+    }
+}
