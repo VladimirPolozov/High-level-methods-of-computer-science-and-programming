5c1d4a8c2ec293de879912574445cf20bc675efa
inkass <Ponovadim67@mail.ru>
2025-10-27 15:31:36 +0300

    change AccessRequest.kt and ExitCode.kt

--- src/domain/dto/AccessRequest.kt
+++ src/domain/dto/AccessRequest.kt
@@ -1,7 +1,5 @@
 // Data class: login, password, path, action: Action, volume: Int
 
-import
-
 data class AccessRequest (
     val login: String,
     val password: String,
--- src/domain/enums/ExitCode.kt
+++ src/domain/enums/ExitCode.kt
@@ -2,18 +2,12 @@
 
 enum class ExitCode(val code: Int) {
     SUCCESS(0),
+    HELP(1),
+    INVALID_PASSWORD(2),
+    INVALID_LOGIN(3),
+    UNKNOWN_ACTION(4),
+    FORBIDDEN(5),
+    NOT_FOUND(6),
     INVALID_FORMAT(7),
-    UNAUTHORIZED(2),
-    USER_NOT_FOUND(3),
-    INVALID_ACTION(4),
-    PERMISSION_DENIED(5),
-    RESOURCE_NOT_FOUND(6),
-    INVALID_VOLUME(7),
-    VOLUME_EXCEEDED(8);
-
-    companion object {
-        fun exit(code: ExitCode): Nothing {
-            kotlin.system.exitProcess(code.code)
-        }
-    }
+    EXCEED_MAX_VOLUME(8)
 }
