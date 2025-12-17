3ed646bb3dd738dbb4dd05518f21aff0182a12fe
Владимир Полозов <57739258+VladimirPolozov@users.noreply.github.com>
2025-12-17 10:11:03 +0700

    Delete task005.kt

--- task005.kt
+++
@@ -1,6 +0,0 @@
-fun main(args: Array<String>) {
-    args.groupingBy { it }.eachCount().entries.toList().sortedWith (
-        compareByDescending<Map.Entry<String, Int>> { it.value }.thenBy { it.key }
-    ).forEach { (word, wordCount) ->
-        println("$word - $wordCount") }
-}
